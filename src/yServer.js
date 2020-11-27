bc1b47592f41c017afc04ed23c635f35ada9e146
Ross Leitch <professional.balbatross@gmail.com>
2020-11-27 16:59:37 +1300

    Public projects route

--- src/yServer.js
+++ src/yServer.js
@@ -4,22 +4,36 @@
  * @type {any}
  */
 const WebSocket = require('ws')
+const https = require('https')
 const http = require('http')
+
 const wss = new WebSocket.Server({ noServer: true })
 const setupWSConnection = require('y-websocket/bin/utils.js').setupWSConnection
+
+const options = {
+  key: fs.readFileSync(__dirname + "../greenlock.d/live/api.rainbowkereru.com/privkey.pem"),
+  cert: fs.readFileSync(__dirname + "../greenlock.d/live/api.rainbowkereru.com/cert.pem")
+}
 
 const port = process.env.PORT || 1234
 
 module.exports = (port) => {
 
-const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('okay')
-})
+  const sslServer = https.createServer(options, (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plan'})
+    res.end('okay')
+  })
+
+  const server = http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain' })
+    response.end('okay')
+  })
 
 wss.on('connection', setupWSConnection)
 
-server.on('upgrade', (request, socket, head) => {
+  //server.on('upgrade', (req, 
+
+sslServer.on('upgrade', (request, socket, head) => {
   // You may check auth of request here..
   /**
    * @param {any} ws
@@ -30,7 +44,7 @@ server.on('upgrade', (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, handleAuth)
 })
 
-server.listen(port)
+sslServer.listen(port)
 
   console.log('running on port', port)
 }
